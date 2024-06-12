96350d84a068e69bce045be9489e21497b6b5c09
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-12 21:33:14 +0530

    add completed

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ const {travelmodel} = require("./models/travel")
 const app=express()
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect("mongodb+srv://thasneemazeez:thasneem38@cluster0.uk9okno.mongodb.net/traveldb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add",(req,res)=>{
+    let input=req.body
+    console.log(input)
+})
+
+app.listen(8080,()=>{
+    console.log("server started")
+})
